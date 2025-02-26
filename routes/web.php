086168055efa7de5184940dbb37a5c6c9e524166
Renssus <74822300+Renssus@users.noreply.github.com>
2025-02-26 18:38:15 +0100

    Filament download

--- routes/web.php
+++ routes/web.php
@@ -9,13 +9,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [HomeController::class, 'index'])->name('home');
 
 
-Route::get('/admin', function () {
-    return view('admin.admin_home_page');
-});
+// Route::get('/admin', function () {
+//     return view('admin.admin_home_page');
+// });
 
-Route::get('/admin/announcements', function () {
-    return view('admin.announcements.index');
-});
+// Route::get('/admin/announcements', function () {
+//     return view('admin.announcements.index');
+// });
 
 
 Route::get('/announcements', [AnnouncementController::class, 'index'])->name('announcements.index');
